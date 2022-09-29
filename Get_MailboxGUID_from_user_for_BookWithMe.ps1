a81d34f96504b348c0919c67054008e850b0849c
Loryan Strant <51473494+loryanstrant@users.noreply.github.com>
2022-09-30 09:05:00 +1000

    Update Get_MailboxGUID_from_user_for_BookWithMe.ps1

--- Get_MailboxGUID_from_user_for_BookWithMe.ps1
+++ Get_MailboxGUID_from_user_for_BookWithMe.ps1
@@ -8,10 +8,10 @@ try {
     $tenantName = "YOURTENANTNAME.onmicrosoft.com"
     $connection = Get-AutomationConnection -Name 'AzureRunAsConnection'
     Connect-ExchangeOnline -CertificateThumbprint $connection.CertificateThumbprint -AppId $connection.ApplicationID -Organization $tenantName
-	 $userMbx = Get-Mailbox $UPN
-	 $userExchangeGuid = $userMbx.ExchangeGuid -replace '-'
-	 Write-Output $userExchangeGuid
-	 disconnect-ExchangeOnline
+    $userMbx = Get-Mailbox $UPN
+    $userExchangeGuid = $userMbx.ExchangeGuid -replace '-'
+    Write-Output $userExchangeGuid
+    disconnect-ExchangeOnline
 }
 catch {
     Write-Error "Unable to add retrieve Mailbox GUID for $UPN"
