0eec1ea2aba9a57b6cde5578323c515ad774c6ac
Loryan Strant <51473494+loryanstrant@users.noreply.github.com>
2022-09-30 09:04:11 +1000

    PowerShell script for Azure Automation Runbook

---
+++ Get_MailboxGUID_from_user_for_BookWithMe.ps1
@@ -0,0 +1,18 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$UPN
+)
+$ErrorActionPreferance = "Stop"
+
+try {
+    $tenantName = "YOURTENANTNAME.onmicrosoft.com"
+    $connection = Get-AutomationConnection -Name 'AzureRunAsConnection'
+    Connect-ExchangeOnline -CertificateThumbprint $connection.CertificateThumbprint -AppId $connection.ApplicationID -Organization $tenantName
+	 $userMbx = Get-Mailbox $UPN
+	 $userExchangeGuid = $userMbx.ExchangeGuid -replace '-'
+	 Write-Output $userExchangeGuid
+	 disconnect-ExchangeOnline
+}
+catch {
+    Write-Error "Unable to add retrieve Mailbox GUID for $UPN"
+}
